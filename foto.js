d9f7641d7b4f197444a9f403c5a4eef2b73dc979
Mutlu Akbas <mutlu.akbas1.ma@gmail.com>
2022-03-14 16:09:56 +0300

    foto degistirme yapıldı

---
+++ foto.js
@@ -0,0 +1,12 @@
+function Degistir() {
+
+
+    var element = document.getElementById('benimfoto');
+    element.src = "donkey-posing.jpg";
+
+
+    var element = document.getElementById('message');
+    element.innerHTML="İyisin Fotoğraf Değiştirici";
+
+
+}
